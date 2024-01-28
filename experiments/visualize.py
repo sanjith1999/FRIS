bf1a0efb7b1f8b25fbf38eeff98f2fbbc40af98b
ishrath99 <ishrathahamed0@gmail.com>
2024-01-28 13:40:48 +0530

    building blocks for creating bead dataset

---
+++ experiments/visualize.py
@@ -0,0 +1,115 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def visualize_A(A, nx, ny, nz, np, m, df, figsize=(8, 8)):
+    plt.figure(figsize=figsize)
+    plt.imshow(A, cmap='viridis')
+    # vertical lines
+    for col in range(nx*ny, nx*ny*nz, nx*ny):
+        plt.axvline(x=col + 0.5, color='red', linestyle='--', linewidth=1)
+    # horizontal lines
+    for row in range(int(nx*df*ny*df), int(nx*df*ny*df*m*np), int(nx*df*ny*df)):
+        plt.axhline(y=row + 0.5, color='blue', linestyle='--', linewidth=1)
+    plt.title('Measurement matrix A')
+    plt.show()
+
+def visualize_X(X, nx, ny, nz, figsize=(15, 15), planes_to_plot=None):
+    if planes_to_plot is None:
+        planes_to_plot = range(nz)
+    num_subplots = len(planes_to_plot)
+
+    num_cols = num_subplots # int(np.ceil(num_subplots / 2))
+    num_rows = 1            # int(np.ceil(num_subplots / num_cols))
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, plane_idx in enumerate(planes_to_plot):
+        plane_data = X[plane_idx*nx*ny : (plane_idx+1)*nx*ny, 0].reshape((ny, nx))
+        axes[i].imshow(plane_data, cmap='viridis')
+        axes[i].set_title(f'P {plane_idx+1}')
+    plt.tight_layout()
+    plt.show()
+
+def visualize_y(y, nx, ny, df, m, np, figsize=(15, 15)):
+    num_cols = m
+    num_rows = np
+
+    nx = int(nx*df)
+    ny = int(ny*df)
+    step = ny*nx
+
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i in range(m):
+        for j in range(np):
+            index = i*np*step + j*step
+            plane_data = y[index : index+step].reshape((ny, nx))
+            axes[i*np + j].imshow(plane_data, cmap='viridis')
+            axes[i*np + j].set_title(f'm {i + 1}, P #{j + 1}')
+    plt.tight_layout() 
+    plt.show()
+
+def obj_mse_twist(obj_twist, times_twist, mse_twist, figsize=(15, 15)):
+    plt.figure(figsize=figsize)
+
+    plt.subplot(1, 2, 1)
+    plt.semilogy(times_twist, obj_twist, 'r', linewidth=2)
+    plt.ylabel('Obj. function')
+    plt.xlabel('CPU time (sec)')
+    plt.grid()
+
+    plt.subplot(1, 2, 2)
+    plt.plot(times_twist[1:], mse_twist[1:], 'r', linewidth=2)
+    plt.ylabel('MSE')
+    plt.xlabel('CPU time (sec)')
+    plt.show()
+
+    print(f'TwIST CPU time: {times_twist[-1]}')
+    print(f'MSE Loss: {mse_twist[-1]:.4e}')
+
+def comparison2(X, xhat, nx, ny, nz, figsize=(15, 15), planes_to_plot=None):
+    if planes_to_plot is None:
+        planes_to_plot = range(nz)
+    num_subplots = len(planes_to_plot)*2
+
+    num_cols = len(planes_to_plot) 
+    num_rows = 2
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, plane_idx in enumerate(planes_to_plot):
+        plane_X = X[plane_idx*nx*ny : (plane_idx+1)*nx*ny, 0].reshape((ny, nx))
+        axes[i].imshow(plane_X, cmap='viridis')
+        axes[i].set_title(f'P {plane_idx+1}')
+
+        plane_xtwist = xhat[plane_idx*nx*ny : (plane_idx+1)*nx*ny, 0].reshape((ny, nx))
+        axes[i+num_cols].imshow(plane_xtwist, cmap='viridis')
+
+    plt.tight_layout()
+    plt.show()
+
+
+def comparison3(xhat1, X, xhat2, nx, ny, nz, figsize=(15, 15), planes_to_plot=None):
+    if planes_to_plot is None:
+        planes_to_plot = range(nz)
+    num_subplots = len(planes_to_plot)*3
+
+    num_cols = len(planes_to_plot) 
+    num_rows = 3
+    fig, axes = plt.subplots(num_rows, num_cols, figsize=figsize)
+    axes = axes.flatten()
+    for i, plane_idx in enumerate(planes_to_plot):
+        plane_xhat1 = xhat1[plane_idx*nx*ny : (plane_idx+1)*nx*ny, 0].reshape((ny, nx))
+        axes[i].imshow(plane_xhat1, cmap='viridis')
+        axes[i].set_title(f'P {plane_idx+1}')
+
+        plane_X = X[plane_idx*nx*ny : (plane_idx+1)*nx*ny, 0].reshape((ny, nx))
+        axes[i+num_cols].imshow(plane_X, cmap='viridis')
+
+        plane_xhat2 = xhat2[plane_idx*nx*ny : (plane_idx+1)*nx*ny, 0].reshape((ny, nx))
+        axes[i+2*num_cols].imshow(plane_xhat2, cmap='viridis')
+
+    plt.tight_layout()
+    plt.show()
+
+
+
