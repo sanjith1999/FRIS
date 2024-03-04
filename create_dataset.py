82192b26b2be15da340aa24d23b849f5abbda67d
Sanjith <sanjithnanthini@gmail.com>
2024-03-04 22:09:51 +0530

    V0.31: Dataset code for blood cell & Neural data.

--- create_dataset.py
+++ create_dataset.py
@@ -4,6 +4,7 @@ from tqdm import tqdm
 from libs.forward_lib.physical_model import PhysicalModel, psf_model
 from libs.forward_lib.linearized_process import LinearizedModel
 from libs.forward_lib.simulate_data import MnistSimulator
+from libs.forward_lib.read_data import ReadData
 
 
 def field_related_calculations():
@@ -66,7 +67,7 @@ def create_A():
     PhysicalModel.ep_dx, PhysicalModel.ep_dy = 2., 2.
 
     LM = LinearizedModel(nx,ny,nz,n_patterns,dd_factor)
-    for IT in range(1, 8):
+    for IT in range(8, 16):
         print(f"\n\nITERATION: {IT+1}\n-------------\n")
         LM.nx, LM.ny, LM.nz, LM.n_patterns, LM.dd_factor = nx, ny, nz, n_patterns, dd_factor
         LM.dx, LM.dy, LM.dz = PhysicalModel.dx, PhysicalModel.dy, PhysicalModel.dz
@@ -91,11 +92,12 @@ def approximate_A():
     dd_factor = 2
     PSF_IT = 51
     PhysicalModel.dx, PhysicalModel.dy, PhysicalModel.dz = 1., 1., 1.
+    PhysicalModel.ep_dx, PhysicalModel.ep_dy = 2., 2.
 
     LM = LinearizedModel(nx,ny,nz,n_patterns,dd_factor)
     LM.init_models(PSF_IT)
     print(LM)
-    for IT in range(0,32):
+    for IT in range(16):
         print(f"\n\nITERATION: {IT+1}\n-------------\n")
         LM.PM.dmd.recover_patterns(IT)
         LM.PM.dmd.visualize_patterns()
@@ -105,7 +107,7 @@ def approximate_A():
 
 # Stack up the individually calculated A to form the larger matrix
 def stack_up_A(DT = 130):
-    n_p = 16
+    n_p = 32
     LM = LinearizedModel()
     stacked_A = torch.tensor([]).to(LM.device)
     for IT in range(int(n_p/2)):
@@ -117,27 +119,44 @@ def stack_up_A(DT = 130):
 
 
 # Create and store objects
-def create_data(IT = 0, batch_size = 2):
+def create_data(IT = 0, batch_size = 2, object_type = "MNIST"):
     nx, ny, nz = 128, 128, 32
     device = 'cuda'
-    F_MS = MnistSimulator(nx, ny, nz, up_factor=(1,4,4))
     X_r, X = torch.tensor([]).to(device), torch.tensor([]).to(device)
+    if object_type == "MNIST":
+        F_MS = MnistSimulator(nx, ny, nz, up_factor=(1,4,4))
 
-    for b in tqdm(range(batch_size), desc = f"Data Point {IT:03}: "):
-        F_MS.update_data()
-        F_MS.reduce_dimension()
-        x, x_r  = F_MS.X.flatten(),  F_MS.X_r.flatten()
+        for b in tqdm(range(batch_size), desc = f"Data Point {IT:03}: "):
+            F_MS.update_data()
+            F_MS.reduce_dimension()
+            x, x_r  = F_MS.X.flatten(),  F_MS.X_r.flatten()
 
-        X_r, X = torch.cat([X_r, x_r.unsqueeze(0)], dim=0), torch.cat([X,x.unsqueeze(0) ])
+            X_r, X = torch.cat([X_r, x_r.unsqueeze(0)], dim=0), torch.cat([X,x.unsqueeze(0) ])
     
+    if object_type == "BLOOD_CELL":
+        RD = ReadData(nx, ny, nz)
+        for b in tqdm(range(batch_size), desc=f"Data Point {IT:03}: "):
+            RD.load_object('blood_cell')
+            RD.reduce_dimension()
+            x, x_r = RD.X.flatten(), RD.X_r.flatten()
 
+            X_r, X = torch.cat([X_r, x_r.unsqueeze(0)], dim=0), torch.cat([X,x.unsqueeze(0) ])
+    
+    if object_type == "NEURAL_CELL":
+        RD = ReadData(nx, ny, nz)
+        for b in tqdm(range(batch_size), desc=f"Data Point {IT:03}: "):
+            RD.load_object('neural_cell')
+            RD.reduce_dimension()
+            x, x_r = RD.X.flatten(), RD.X_r.flatten()
+
+            X_r, X = torch.cat([X_r, x_r.unsqueeze(0)], dim=0), torch.cat([X,x.unsqueeze(0) ])
     torch.save(X_r,f"./data/dataset/object/X_r_{IT}.pt")
     torch.save(X,f"./data/dataset/h_object/X_{IT}.pt")
 
 
 # Calculate Measurement
 def run_process():
-    for m_it in range(8):
+    for m_it in range(16):
         LM = LinearizedModel()
         LM.load_matrix(m_it)
         for IT in tqdm(range(32), desc = f"Pattern Pair: {m_it+1:02}\t\tData Point: "):
@@ -158,14 +177,18 @@ def run_process():
 def combine_data():
     for IT in range(32):
         Yi = torch.load(f"./data/dataset/measurement/Y_{IT}.pt")
-        Xi = torch.load(f"./data/dataset/object/X_r_{IT}.pt")
+        Xri = torch.load(f"./data/dataset/object/X_r_{IT}.pt")
+        Xi = torch.load(f"./data/dataset/h_object/X_{IT}.pt")
         if IT ==0:
             S_Y = Yi
-            S_Xr = Xi
+            S_Xr = Xri
+            S_X = Xi
         else:
             S_Y = torch.cat((S_Y, Yi))
-            S_Xr = torch.cat((S_Xr, Xi))
+            S_Xr = torch.cat((S_Xr, Xri))
+            S_X = torch.cat((S_X, Xi))
     print("Object:", S_Xr.shape,"Measurement:",S_Y.shape)
+    torch.save(S_X, "./data/dataset/h_object/S_X.pt")
     torch.save(S_Xr, "./data/dataset/object/S_Xr.pt")
     torch.save(S_Y, "./data/dataset/measurement/S_Y.pt")
         
